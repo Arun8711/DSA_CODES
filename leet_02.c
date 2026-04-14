08ee69ebcdbd68849a48875f0be7a1d5e06f3ca7
Arun_Halagali <01fe24bec355@kletech.ac.in>
2026-04-14 11:49:06 +0530

    Update and rename lee.c to leet_02.c

--- leet_02.c
+++ leet_02.c
@@ -1,3 +1,8 @@
+/*Insert all given Router IDs into a Binary Search Tree (BST).
+If any input is -1, print "Invalid input" and stop.
+Insert the new Router ID into the BST.
+Perform inorder traversal (Left → Root → Right).
+Print the routing table in sorted ascending order.*/
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct node
